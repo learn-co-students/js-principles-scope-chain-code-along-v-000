8181abb13d52e59e089a8fc06c978b5a99a9fe37
Julian Johannesen <julian.johannesen@gmail.com>
2018-02-26 19:02:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 var customerName = 'bob'
 const leastFavoriteCustomer = 'sour fred'
+//bestCustomer gets declared in global scope
 
 function upperCaseCustomerName() {
   return customerName = customerName.toUpperCase()
 }
 
 function changeLeastFavoriteCustomer(){
+  //This will throw an error b/c leastFavoriteCustomer is declared with const
   leastFavoriteCustomer = 'sour fred'
 }
 
 
 function setBestCustomer(){
+  //bestCustomer isn't declared, so the engine declares it in global scope
   bestCustomer = 'not bob'
 }
 
@@ -19,6 +22,9 @@ function overWriteBestCustomer(newFavorite){
 }
 
 function congratulateCustomer(){
+  //favoriteCustomer is no available to congratulateCustomer
+  //For this to work, congratulateCustomer would have to occur in the scope of
+  //attemptTwoFavoriteCustomers
   return `congrats ${favoriteCustomer}`
 }
 
