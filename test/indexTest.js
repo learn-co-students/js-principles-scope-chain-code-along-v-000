04b2847782adceb955af3d969e288bb8d1c5ef21
Jordan Kline <jordan.kline@gmail.com>
2018-04-29 17:54:17 -0700

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,5 @@
 const expect = chai.expect;
+
 describe('Fix the Scope', function() {
   describe('declare customerName to be bob in global scope', function() {
     it('returns the customerName', () => {
