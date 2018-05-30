cf25e8a79e4a46aece256afebfdfb5d2077e0210
Joanne Lee <vernapii@gmail.com>
2018-05-29 19:15:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-var customerName = 'bob'
-const leastFavoriteCustomer = 'sour fred'
+var customerName = "bob"
 
-function upperCaseCustomerName() {
-  return customerName = customerName.toUpperCase()
+function upperCaseCustomerName () {
+  customerName = customerName.toUpperCase()
 }
 
-function changeLeastFavoriteCustomer(){
-  leastFavoriteCustomer = 'sour fred'
-}
-
-
-function setBestCustomer(){
+function setBestCustomer() {
   bestCustomer = 'not bob'
 }
 
-function overWriteBestCustomer(newFavorite){
-  bestCustomer = newFavorite
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob'
 }
 
-function congratulateCustomer(){
-  return `congrats ${favoriteCustomer}`
+const leastFavoriteCustomer = 'jojo'
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'titi'
 }
 
-function attemptTwoFavoriteCustomers(){
-    let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "hehe"
+  let favoriteCustomer = "lele"
 }
