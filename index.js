389bd926ccc29d7b6bad9f15f8ad6572fd77a680
Dee Race <deerace12@gmail.com>
2018-07-27 15:41:29 +0000

    Done.

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function overWriteBestCustomer(newFavorite){
 
 function congratulateCustomer(){
   return `congrats ${favoriteCustomer}`
-}
+};
 
 function attemptTwoFavoriteCustomers(){
     let favoriteCustomer = 'bar'
     let favoriteCustomer = 'not bar'
-}
+};
