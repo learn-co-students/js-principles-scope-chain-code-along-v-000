1c0d4f39eaa93e424111106effb1e71ff4371fde
ivillicana <31767556+ivillicana@users.noreply.github.com>
2018-08-02 15:27:56 -0400

    Done.

--- index.js
+++ index.js
@@ -18,11 +18,7 @@ function overWriteBestCustomer(newFavorite){
   bestCustomer = newFavorite
 }
 
-function congratulateCustomer(){
-  return `congrats ${favoriteCustomer}`
-}
-
 function attemptTwoFavoriteCustomers(){
-    let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
+  let favoriteCustomer = 'bar'
+  let favoriteCustomer = 'soemthing'
 }
