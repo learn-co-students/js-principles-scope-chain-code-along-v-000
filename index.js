cf5cda1b0b7bd0a12f448437ff0ec59c457a2d51
Aspen James <aspenjames@tqca.org>
2018-10-01 14:31:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-/* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  return bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'bob';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'not bob';
+}
+
+// function attemptTwoFavoriteCustomers() {
+//   let favoriteCustomer;
+//   let favoriteCustomer;
+// }
