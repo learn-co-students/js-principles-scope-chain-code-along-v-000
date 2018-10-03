433a8693fff827ac920e49ab288d9811f0122bc4
Sara Stanton <sara.mpa13@gmail.com>
2018-10-03 04:15:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer(newName) {
+  bestCustomer = newName;
+}
+
+const leastFavoriteCustomer = "ethel"
+
+function changeLeastFavoriteCustomer(newName) {
+  leastFavoriteCustomer = newName;
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "lou ann"
+  let favoriteCustomer = "betty sue";
+}
