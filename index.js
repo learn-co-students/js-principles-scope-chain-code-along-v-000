881c576580f7ca8a1620cc1ab100c2497585503a
Daniel Hernandez <elreymcd@yahoo.com>
2018-10-06 09:06:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 /* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(name){
+    bestCustomer = name;
+}
+
+const leastFavoriteCustomer = 'Daniel';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'
+}
+
+let favoriteCustomer = 'Mina';
+
+function attemptTwoFavoriteCustomers(){
+    let favoriteCustomer = 'Ariana';
+    let favoriteCustomer = 'Mikel'
+}
