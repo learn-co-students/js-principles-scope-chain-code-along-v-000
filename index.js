737c68bedf2f29abadccb5ef1c6be03f2b2c9079
Lauren Perchitti <laurenperchitti@gmail.com>
2018-10-08 01:18:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+var customerName = 'bob'
+const leastFavoriteCustomer = 'sour fred'
+
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+ bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'lauren'
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'bob'
+  let favoriteCustomer = 'not bob'
+}
