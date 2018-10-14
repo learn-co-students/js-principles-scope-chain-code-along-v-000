c6628ebc69403544d9fb35321e3988e1d0e85cd2
Natalie Halawani <hala1224@gmail.com>
2018-10-14 18:31:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+var customerName ='bob';
+
+function upperCaseCustomerName() {
+  customerName=customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer='not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer='maybe bob';
+}
+const leastFavoriteCustomer='Ted';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer='Jack';
+}
+ 
+ function attemptTwoFavoriteCustomers () {
+   const favoriteCustomer = 'Jan';
+   let favoriteCustomer='Nat';
+   
+ }
