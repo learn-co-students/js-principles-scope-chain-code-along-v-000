545f467732a06be2975ec5fe0714d1c509a8ecac
asabogal <asabogal.git@gmail.com>
2018-10-19 12:26:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 /* Your code goes here */
+var customerName = 'bob'
+const leastFavoriteCustomer = 'someone'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+};
+
+function setBestCustomer() {
+   bestCustomer = 'not bob'
+};
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+};
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'other'
+};
+
+function attemptTwoFavoriteCustomers() {
+  const favoriteCustomer = 'favorite'
+  const favoriteCustomer ='another favorite'
+};
