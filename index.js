74eaca8ebcad66ad908ee9470799a279ef704eb5
pracniscate <lorena.giddens@outlook.com>
2018-10-21 15:45:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-/* Your code goes here */
+var customerName = 'bob'; // global scope
+const leastFavoriteCustomer = 'some initial value';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'blah';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'alice';
+  // let favoriteCustomer = 'not alice';
+}
