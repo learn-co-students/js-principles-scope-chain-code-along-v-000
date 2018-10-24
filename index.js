08a22ca6a7226e31973a41a71ce732d1578c4a27
kylek <27875726+kylekinnear@users.noreply.github.com>
2018-10-24 12:32:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 /* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'definitely not bob';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'it was bob all along'
+}
+
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'bob';
+  let favoriteCustomer = 'joey';
+}
