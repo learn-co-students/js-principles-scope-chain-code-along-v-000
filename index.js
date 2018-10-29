dee9d0f850670a4dd5d175c11fd26dc850765b65
Gabriela Suriano <surianogabriela@gmail.com>
2018-10-29 21:35:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 /* Your code goes here */
+var customerName = 'bob';
+ function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+};
+ function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+ function overWriteBestCustomer(){
+  bestCustomer = "maybe bob";
+}
+ const leastFavoriteCustomer = 'phil';
+ function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'sally';
+}
+ function attemptTwoFavoriteCustomers(){
+  let FavoriteCustomer = 'bill';
+  let FavoriteCustomer = 'hal';
+}
