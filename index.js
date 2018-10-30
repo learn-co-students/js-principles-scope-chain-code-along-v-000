9caa467f1c61ff296e688a54835addf9c471f57a
jokerdelta <jokerdelta@yahoo.com>
2018-10-29 19:23:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 /* Your code goes here */
+// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+var leastFavoriteCustomer;
+var favoriteCustomer = 'bob';
+
+function upperCaseCustomerName() {
+
+  customerName = customerName.toUpperCase()
+
+  return customerName;
+}
+
+function setBestCustomer(){
+
+  bestCustomer = 'not bob';
+
+  return bestCustomer;
+
+}
+
+function overWriteBestCustomer(){
+
+  bestCustomer = 'maybe bob';
+
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer.name;
+}
+
+function attemptTwoFavoriteCustomers(){
+
+  let favoriteCustomer = 'Jerry';
+  let favoriteCustomer = 'Jack';
+
+
+}
