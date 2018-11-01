4e43394d1701f661a4cda4c7b5afc0891252ec24
Jonathan Chan <johnkchan252@gmail.com>
2018-11-01 03:05:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 /* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'jon';
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'test';
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'test';
+  let favoriteCustomer = 'test';
+}
