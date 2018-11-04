3b6e74d7cbda9ffb0272d7ee966b7bd51535717b
BrettSWeisberg <weisberg.brett@gmail.com>
2018-11-04 11:15:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 /* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName (){
+   return customerName = customerName.toUpperCase();
+}
+
+ bestCustomer;
+
+function setBestCustomer () {
+     bestCustomer = 'not bob';
+
+    return bestCustomer;
+}
+
+
+function overWriteBestCustomer(name) {
+   bestCustomer = name
+}
+
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = 'brett';
+
+  leastFavoriteCustomer = 'bob';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let twoFavoriteCustomers = 'bob';
+  let twoFavoriteCustomers = 'tom';
+}
