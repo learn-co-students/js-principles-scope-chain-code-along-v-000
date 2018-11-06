e551940abd2ae0574d244f4012f5dc2361b74036
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-06 05:40:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase()
+  return customerName
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = "Jim"
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "Jobin"
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = "Jobin"
+  let favoriteCustomer = "Josh"
+}
