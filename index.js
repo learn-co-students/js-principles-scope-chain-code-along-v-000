1ae69260558438402eaf35218e6548d9c5d23fd4
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-11-26 11:29:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 /* Your code goes here */
+var customerName = 'bob';
+const leastFavoriteCustomer = 'robin';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+function reassignLeastFavorite() {
+  changeLeastFavoriteCustomer = 'nhinh';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'nhinh';
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer =  'robin';
+  let favoriteCustomer =  'nhinh';
+}
