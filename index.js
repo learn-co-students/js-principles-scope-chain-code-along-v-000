4a16b7edec38ee5dc29c789d27aad6e867ecd03f
github username <github email address>
2018-12-08 15:47:20 -0500

    Done.

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function changeLeastFavoriteCustomer() {
 }
 
 function attemptTwoFavoriteCustomers() {
-  let favoriteCustomer = 'chris';
-  let favoriteCustomer = 'david';
+  var favoriteCustomer = 'chris';
+  var favoriteCustomer = 'david';
 }
