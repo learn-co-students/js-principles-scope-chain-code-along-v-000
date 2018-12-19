a9be375eb094bb571d187bf6c3a487d16d677bcd
Kachel Wittig <kjwittig@gmail.com>
2018-12-19 14:04:07 -0600

    Donezo

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-/* Your code goes here */
+var customerName = "bob";
+const leastFavoriteCustomer = "rocky";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "lily";
+}
+
+// function attemptTwoFavoriteCustomers() {
+//   let favoriteCustomer = 'mohammed';
+//   let favoriteCustomer = 'kachel';
+// }
