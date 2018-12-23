f96de866dfd346cd3b486736708516e7871f318d
SMartinSci <sm674@cornell.edu>
2018-12-23 15:21:10 -0800

    Done.

--- index.js
+++ index.js
@@ -14,15 +14,7 @@ function setBestCustomer() {
     bestCustomer = 'not bob'
 }
 
-function overWriteBestCustomer() {
-    bestCustomer = 'maybe bob'
+function overWriteBestCustomer(newFavorite) {
+    bestCustomer = newFavorite
 }
 
-function congratulateCustomer() {
-    return `congrats ${favoriteCustomer}`
-}
-
-function attemptTwoFavoriteCustomers() {
-    let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
-}
