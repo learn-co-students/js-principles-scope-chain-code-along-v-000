822360a2fdd04acd2355b8d97c43fc02f0d5d58d
Brenden Thornton <thorntonbrenden@gmail.com>
2019-01-15 11:24:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'brenden';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'benji';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'Mariana';
+  let favoriteCustomer = 'Noa';
+}
