f20dcf86aa12c06132808b7b5372ab42ca304375
lauradarlak <lauradarlak@gmail.com>
2019-01-17 10:39:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 /* Your code goes here */
+
+var customerName = 'bob';
+const leastFavoriteCusomer = 'sam';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCusomer = 'mac';
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'tom';
+  let favoriteCustomer = 'max';
+}
