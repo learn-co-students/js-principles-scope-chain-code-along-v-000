c3a49f9d1c872c56586100f5c2f610d775886225
freeWillee <will.ik.lee@gmail.com>
2019-01-18 10:58:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 /* Your code goes here */
+var customerName = 'bob';
+
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(name){
+    bestCustomer = name;
+}
+
+const leastFavoriteCustomer = 'bobby';
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'something else';
+}
+
+function attemptTwoFavoriteCustomers(){
+    let favoriteCustomer = 'bobbyboy';
+    let favoriteCustomer = 'terence';
+}
