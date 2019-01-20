7ce7c08212cf543fba60f62b07a8bcd4011b2931
github username <github email address>
2019-01-19 17:29:04 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 /* Your code goes here */
+var customerName = 'bob'
+const leastFavoriteCustomer = 'no one'
+
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase()
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+function overWriteBestCustomer(someone) {
+  bestCustomer = someone
+}
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'me'
+}
+function attemptTwoFavoriteCustomers(){
+    let favoriteCustomer = 'you'
+    // let favoriteCustomer = 'not you'
+    throw new SyntaxError('Hello', 'someFile.js', 10);
+}
