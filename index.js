514acd88ee0cbfde3b5f2c9ce4526d397a4994f8
amandena <manda.panda91@yahoo.com>
2019-02-02 12:05:30 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 /* Your code goes here */
+var customerName = 'bob';
+const leastFavoriteCustomer = 'piero'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'amanda';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'amanda';
+  let favoriteCustomer = 'piero';
+}
