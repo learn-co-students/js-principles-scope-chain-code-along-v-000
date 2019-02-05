1a614f7a0100a268713ba5e9ce339cc3c5e117b6
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-05 21:44:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-/* Your code goes here */
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  return bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer() {
+  return bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+  const leastFavoriteCustomer = "bruce";
+  leastFavoriteCustomer = "bob";
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "bruce";
+  let favoriteCustomer = "bob";
+}
