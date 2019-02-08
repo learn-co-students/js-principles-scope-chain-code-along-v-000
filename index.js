d002fed386e57d695dd2caea9ffacfcaa4db601d
Christine Cua <cloudycua@gmail.com>
2019-02-08 17:52:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-/* Your code goes here */
+var customerName = 'bob';
+const leastFavoriteCustomer = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bub';
+}
+
+// not allowed to declare favoriteCustomer twice with let
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'jack';
+  let favoriteCustomer = 'jill';
+}
