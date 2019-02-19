efb9da8301335566eafba6a9dc16a40e61be9fb2
Richard Sanchez <richard.charles.s@gmail.com>
2019-02-19 11:24:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+var customerName = 'bob'
+
+function upperCaseCustomerName () {
+   customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer () {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'Richard';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Kitty';
+}
+
+function attemptTwoFavoriteCustomers () {
+  let favoriteCustomer = 'Judge';
+
+  let favoriteCustomer = 'Stella';
+}
