551e855a8fd9736006ce9220040b9477785a487c
Leigh Passamano <leighayna.josele@gmail.com>
2019-02-25 19:05:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 /* Your code goes here */
+var customerName = 'bob'
+const leastFavoriteCustomer = 'sour fred'
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer(name) {
+  bestCustomer = name
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'bad barry'
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'leigh'
+  let favoriteCustomer = 'chaz'
+}
