ce3e2e281f433ab0efd027b522807b41767e24a6
Rebecca Strauss <rstrauss127@gmail.com>
2019-03-05 18:55:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
 var customerName = "bob";
-
-function upperCaseCustomerName() {
-  customerName = "BOB";
-}
-
-function setBestCustomer() {
-  var bestCustomer = "not bob";
-}
-
-function overWriteBestCustomer() {
-  bestCustomer = "maybe bob";
-}
-
-const leastFavoriteCustomer;
-
-function changeLeastFavoriteCustomer() {
-  
-}
