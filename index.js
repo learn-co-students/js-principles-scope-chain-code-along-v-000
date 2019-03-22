2985ab05855c2a641c7cb1d9fd855eda178a7794
MDAM182 <mdam2345@gmail.com>
2019-03-21 23:56:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-/* Your code goes here */
+var customerName = `bob`
+let favoriteCustomer = 'tim';
+
+
+function upperCaseCustomerName(){
+  customerName = `BOB`
+}
+
+function setBestCustomer(){
+  bestCustomer = `not bob`
+}
+
+function overWriteBestCustomer(){
+  bestCustomer = `maybe bob`
+}
+
+const leastFavoriteCustomer = `bob`
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = `jim`
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'bill';
+  throw SyntaxError()
+}
