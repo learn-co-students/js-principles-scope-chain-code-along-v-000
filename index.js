cdcde0d0a8ba6f13ccb41d68089b3c5ff6e413d7
EdselCaprice <caprice@mail.usf.edu>
2019-04-20 18:24:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 /* Your code goes here */
+
+
+var customerName = 'bob';
+const leastFavoriteCustomer = 'jim';
+
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+   bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer(name) {
+    bestCustomer = name;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'sam';   
+}
+
