2352e57d511fae7e8de8c45a8714b7f64b87cb8e
Noah Summers <summers.noah@gmail.com>
2019-04-27 19:35:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-/* Your code goes here */
+var customerName = "bob";
+const leastFavoriteCustomer = "lemons";
+
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overWriteBestCustomer(message) {
+  bestCustomer = message;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bob";
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer;
+  let favoriteCustomer;
+}
