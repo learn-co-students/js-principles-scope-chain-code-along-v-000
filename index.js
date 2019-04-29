adebe966a4b5b01be1cccaa6e550d550bf1709e1
Yana Ustsinovich <yustinovich@icloud.com>
2019-04-29 20:13:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 /* Your code goes here */
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'bill';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'ben';
+}
+ //function attemptTwoFavoriteCustomers() {
+ //let favoriteCustomer;
+  //favoriteCustomer = 'fin';
+//let favoriteCustomer = 'kin';
+//}
