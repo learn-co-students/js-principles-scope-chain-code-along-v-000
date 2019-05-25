b4e9ebcd1f06f865ebed54a44c1ae71605ca5b19
lpzraf <lpz.raf@gmail.com>
2019-05-25 18:49:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 /* Your code goes here */
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overWriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "hey";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "ho";
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "hola";
+  let favoriteCustomer = "hey";
+}
