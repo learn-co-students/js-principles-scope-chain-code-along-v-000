fddcd403a02b92b150f8d287493a7d92c0e79ffe
Allie Howard <alliemhow@gmail.com>
2020-06-06 06:38:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-/* Your code goes here */
+var customerName = 'bob'
+const leastFavoriteCustomer = 'dan'
+
+function upperCaseCustomerName () {
+  return customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer () {
+  bestCustomer = 'not bob'
+}
+
+function overWriteBestCustomer () {
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer () {
+  leastFavoriteCustomer = 'mike'
+}
+
+// function attemptTwoFavoriteCustomers(){
+//     let favoriteCustomer = 'jim'
+//     let favoriteCustomer = 'john'
+// }
